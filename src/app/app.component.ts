00ba77a6ba031a32d3375be5bb7c194e81510600
Alex Sikand <alex@sikand.org>
2020-08-24 18:21:32 -0400

    Add 8/23/20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,18 +62,18 @@ export class AppComponent {
 
 //public single = [{'name': '8/16/20', 'value': 286}, {'name': '8/17/20', 'value': 1086}, {'name': '8/18/20', 'value': 965}, {'name': '8/19/20', 'value': 1450}, {'name': '8/20/20', 'value': 1414}, {'name': '8/21/20', 'value': 1456}, {'name': '8/22/20', 'value': 1940}]
 
-public single = [{'name': '8/16/20', 'value': 1}, {'name': '8/17/20', 'value': 4}, {'name': '8/18/20', 'value': 4}, {'name': '8/19/20', 'value': 0}, {'name': '8/20/20', 'value': 0}, {'name': '8/21/20', 'value': 3}, {'name': '8/22/20', 'value': 6}]
+public single = [{'name': '8/16/20', 'value': 1}, {'name': '8/17/20', 'value': 4}, {'name': '8/18/20', 'value': 4}, {'name': '8/19/20', 'value': 0}, {'name': '8/20/20', 'value': 0}, {'name': '8/21/20', 'value': 3}, {'name': '8/22/20', 'value': 6}, {'name': '8/23/20', 'value': 0}]
 
-public totals = [{'name': '8/16/20', 'value': 286}, {'name': '8/17/20', 'value': 1086}, {'name': '8/18/20', 'value': 965}, {'name': '8/19/20', 'value': 1450}, {'name': '8/20/20', 'value': 1414}, {'name': '8/21/20', 'value': 1456}, {'name': '8/22/20', 'value': 1940}]
+public totals = [{'name': '8/16/20', 'value': 286}, {'name': '8/17/20', 'value': 1086}, {'name': '8/18/20', 'value': 965}, {'name': '8/19/20', 'value': 1450}, {'name': '8/20/20', 'value': 1414}, {'name': '8/21/20', 'value': 1456}, {'name': '8/22/20', 'value': 1940}, {'name': '8/23/20', 'value': 1278}]
 
-public invalids = [{'name': '8/16/20', 'value': 0}, {'name': '8/17/20', 'value': 13}, {'name': '8/18/20', 'value': 11}, {'name': '8/19/20', 'value': 7}, {'name': '8/20/20', 'value': 4}, {'name': '8/21/20', 'value': 10}, {'name': '8/22/20', 'value': 9}]
+public invalids = [{'name': '8/16/20', 'value': 0}, {'name': '8/17/20', 'value': 13}, {'name': '8/18/20', 'value': 11}, {'name': '8/19/20', 'value': 7}, {'name': '8/20/20', 'value': 4}, {'name': '8/21/20', 'value': 10}, {'name': '8/22/20', 'value': 9}, {'name': '8/23/20', 'value': 8}]
 
-public negatives = [{'name': '8/16/20', 'value': 285}, {'name': '8/17/20', 'value': 1069}, {'name': '8/18/20', 'value': 950}, {'name': '8/19/20', 'value': 1443}, {'name': '8/20/20', 'value': 1410}, {'name': '8/21/20', 'value': 1443}, {'name': '8/22/20', 'value': 1925}]
+public negatives = [{'name': '8/16/20', 'value': 285}, {'name': '8/17/20', 'value': 1069}, {'name': '8/18/20', 'value': 950}, {'name': '8/19/20', 'value': 1443}, {'name': '8/20/20', 'value': 1410}, {'name': '8/21/20', 'value': 1443}, {'name': '8/22/20', 'value': 1925}, {'name': '8/23/20', 'value': 1270}]
 ​
 public cardData = [
   {
     "name": "Isolated",
-    "value": 21
+    "value": 19
   },
   {
     "name": "Non-Contagious",
@@ -81,7 +81,7 @@ public cardData = [
   },
   {
     "name": "Recovered",
-    "value": 3
+    "value": 5
   }
 ]
 
