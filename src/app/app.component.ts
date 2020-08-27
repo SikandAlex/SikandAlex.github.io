1786f344d8db51d74d40aad13ea27415d67b399a
Alex Sikand <alex@sikand.org>
2020-08-27 03:01:39 -0400

    Finish auto-updating from the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,50 @@ import { CovidDataService } from './covid.service';
 })
 export class AppComponent {
 
+  // data goes here
+  public positives = [];
+  public negatives = [];
+  public invalids = [];
+  public totals = [];
+
+  public multi = [];
+
+  public cardData = [];
+
+
+
   constructor(private _covid: CovidDataService) {}
 
   ngOnInit() {
-    this._covid.historicalData()
+
+
+    this._covid.getTotal()
       .subscribe(res => {
-        console.log(res)
+        this.totals = res["data"];
+      });
+    this._covid.getPositive()
+      .subscribe(res => {
+        this.positives = res["data"]
+      });
+    this._covid.getInvalid()
+      .subscribe(res => {
+        this.invalids = res["data"]
+      });
+    this._covid.getNegative()
+      .subscribe(res => {
+        this.negatives = res["data"]
+      });
+    this._covid.getMulti()
+      .subscribe(res => {
+        this.multi = res["data"]
+      });
+    this._covid.getInfectionStatus()
+      .subscribe(res => {
+        this.cardData = res["data"]
+      });
 
-        var names = []
-
-        for (var x in res) {
-          names.push(x)
-        }
-
-
-
-      })
   }
+
 
 
   title = 'BU-COVID19-DASH';
@@ -57,52 +84,9 @@ export class AppComponent {
   //pie
   showLabels = true;
 
-  // data goes here
 
 
-//public single = [{'name': '8/16/20', 'value': 286}, {'name': '8/17/20', 'value': 1086}, {'name': '8/18/20', 'value': 965}, {'name': '8/19/20', 'value': 1450}, {'name': '8/20/20', 'value': 1414}, {'name': '8/21/20', 'value': 1456}, {'name': '8/22/20', 'value': 1940}]
 
-public single = [{'name': '8/16/20', 'value': 1}, {'name': '8/17/20', 'value': 4}, {'name': '8/18/20', 'value': 4}, {'name': '8/19/20', 'value': 0}, {'name': '8/20/20', 'value': 0}, {'name': '8/21/20', 'value': 3}, {'name': '8/22/20', 'value': 6}, {'name': '8/23/20', 'value': 0}]
-
-public totals = [{'name': '8/16/20', 'value': 286}, {'name': '8/17/20', 'value': 1086}, {'name': '8/18/20', 'value': 965}, {'name': '8/19/20', 'value': 1450}, {'name': '8/20/20', 'value': 1414}, {'name': '8/21/20', 'value': 1456}, {'name': '8/22/20', 'value': 1940}, {'name': '8/23/20', 'value': 1278}]
-
-public invalids = [{'name': '8/16/20', 'value': 0}, {'name': '8/17/20', 'value': 13}, {'name': '8/18/20', 'value': 11}, {'name': '8/19/20', 'value': 7}, {'name': '8/20/20', 'value': 4}, {'name': '8/21/20', 'value': 10}, {'name': '8/22/20', 'value': 9}, {'name': '8/23/20', 'value': 8}]
-
-public negatives = [{'name': '8/16/20', 'value': 285}, {'name': '8/17/20', 'value': 1069}, {'name': '8/18/20', 'value': 950}, {'name': '8/19/20', 'value': 1443}, {'name': '8/20/20', 'value': 1410}, {'name': '8/21/20', 'value': 1443}, {'name': '8/22/20', 'value': 1925}, {'name': '8/23/20', 'value': 1270}]
-​
-public cardData = [
-  {
-    "name": "Isolated",
-    "value": 19
-  },
-  {
-    "name": "Non-Contagious",
-    "value": 1
-  },
-  {
-    "name": "Recovered",
-    "value": 5
-  }
-]
-
-public multi = [
-  {
-    "name": "Total",
-    "series": this.totals
-  },
-  {
-    "name": "Positive",
-    "series": this.single
-  },
-  {
-    "name": "Invalid",
-    "series": this.invalids
-  },
-  {
-    "name": "Negative",
-    "series": this.negatives
-  }
-]
 
 
 }
--- src/app/covid.service.ts
+++ src/app/covid.service.ts
@@ -10,9 +10,35 @@ export class CovidDataService {
 
   constructor(private _http: HttpClient) { }
 
-  historicalData() {
-    return this._http.get("https://bu-covid19-api.herokuapp.com/historical")
+  getPositive() {
+    return this._http.get("https://bu-covid19-api.herokuapp.com/ngx/Positive")
       .pipe(map(result => result));
   }
+
+  getNegative() {
+    return this._http.get("https://bu-covid19-api.herokuapp.com/ngx/Negative")
+      .pipe(map(result => result));
+  }
+
+  getInvalid() {
+    return this._http.get("https://bu-covid19-api.herokuapp.com/ngx/Invalid")
+      .pipe(map(result => result));
+  }
+
+  getTotal() {
+    return this._http.get("https://bu-covid19-api.herokuapp.com/ngx/Total")
+      .pipe(map(result => result));
+  }
+
+  getMulti() {
+    return this._http.get("https://bu-covid19-api.herokuapp.com/ngx-all")
+      .pipe(map(result => result));
+  }
+
+  getInfectionStatus() {
+    return this._http.get("https://bu-covid19-api.herokuapp.com/ngx-infections")
+      .pipe(map(result => result));
+  }
+
 
 }
